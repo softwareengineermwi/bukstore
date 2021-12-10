eb46efd174018d9d3abbdf20855174ba2ff2cf5b
Mwila Kaunda <mbkcust@gmail.com>
2021-12-10 15:52:51 +0200

    Setup

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'red'
 import logger from 'redux-logger'
 import booksReducer from './books/books'
 
